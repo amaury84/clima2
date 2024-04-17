91ac44a5794d92d2b672aeb292d4c1d6969ab062
amaury84 <amaury84@gmail.com>
2024-04-17 13:21:26 -0500

    Agregar archivos de proyecto.

---
+++ clima2/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace clima2
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient Client;    
+        public MainPage()
+        {
+            InitializeComponent();
+            Client = new HttpClient();
+        }
+
+        private async void ObtenerCLimaBtn(object sender, EventArgs e)
+        {
+            string city = CityEntry.Text;
+            string apikey = "ec47323ccc0fde17e0219a4f991fb21d";
+            string apiUrl = "api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=ec47323ccc0fde17e0219a4f991fb21d";
+            HttpResponseMessage response = await Client.GetAsync(apiUrl);
+            if ( response.IsSuccessStatusCode)
+            {
+                await DisplayAlert("", "Todo ha salido bien!", "ok");
+            }
+            string weather = await response.Content.ReadAsStringAsync();
+            Clima clima = JsonConvert.DeserializeObject<Clima>(weather);
+            
+            double txtTemp = clima.main.Temperatura - 273.15;
+            WeatherLaber.Text = "Temperatura: "+txtTemp.ToString()+ "°C";
+        }
+        public class Clima
+        {
+            [JsonProperty("main")]
+            public Main main { get; set; }
+        }
+        public class Main 
+        {
+            [JsonProperty("temp")]
+            public double Temperatura {  get; set; }
+        }
+    }
+}
